0444e1d4eb5143f2817eb4ed5d13ce3f27979e00
willownezz <luizdgarcia@gmail.com>
2019-02-21 23:53:29 -0800

    Proj1 start

---
+++ cust_ports.cpp
@@ -0,0 +1,47 @@
+/*-----------------------------------------------------------------------------------------
+FILE NAME:          cust_ports.cpp
+DESCRIPTION:        Financial Portifolio Program
+USAGE:              ./Proj1
+COMPILER:           GNU g++ compiler on Linux
+
+MODIFICATION HISTORY:
+Author                      Date               Version       Details
+-----------------           ----------         --------      ----------------------------
+Luiz Diego Garcia           2019-02-21         0.0.1         Created File
+Luiz Diego Garcia           2019-02-21         0.0.2         Changed description / Headers
+-----------------------------------------------------------------------------------------*/
+
+//Libraries
+#include <iostream>
+
+using namespace std;
+
+//Prototypes
+
+
+//Structures(Blueprints)
+
+
+//Macros
+
+
+//Constant Variables
+const char PROGRAMMER_NAME[] = "Luiz Diego Garcia";
+
+/*-----------------------------------------------------------------------------------------
+FUNCTION:           main()
+DESCRIPTION:        start of the program
+RETURNS:            0
+-----------------------------------------------------------------------------------------*/
+int main()
+{
+    //Variables used in multiple functions
+        
+
+    cout << endl;
+    cout << "Programmed by: " << PROGRAMMER_NAME << " -- ";
+    cout << __DATE__ << "  " __TIME__ << endl;
+    cout << endl;
+
+    return 0;
+}
